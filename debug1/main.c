af1ccf5f9d5cccc11773554ab7833c03e9bf68f0
Maria <xiziximaria@gmail.com>
2025-11-29 22:21:27 +0800

    adding labc

--- debug1/main.c
+++ debug1/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    string name = "Juan";
+    char name[] = "Juan";
     printf("Name: %s\n", name);
     return 0;
 }
 
 /**
 REASONING:
-
+1. strings are written as "char (variable_name)[] = "string_value";"
 
 */
