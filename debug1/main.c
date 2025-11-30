480777eddc479b8b4c8f51c88142094d460493a5
kielelijahastillasa <kielelijahastillasa@gmail.com>
2025-11-30 17:08:53 +0800

    Revert "debugged and added reasoning"
    This reverts commit 166eded7bd427d5a859e91446d3202e2484c6b26.

--- debug1/main.c
+++ debug1/main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    char name [] = "Juan";
+    string name = "Juan";
     printf("Name: %s\n", name);
     return 0;
 }
 
 /**
 REASONING:
-The Data Type we should use is CHAR not STRING. :>
 
 
 */
