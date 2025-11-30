30f4abb7902a8d6d0e2911250b0ae4c6a28a97ac
kielelijahastillasa <kielelijahastillasa@gmail.com>
2025-11-30 15:43:55 +0800

    added debug-2

--- debug2/main.c
+++ debug2/main.c
@@ -4,13 +4,14 @@ int main() {
     int age = 20;
     double gpa = 1.75;
 
-    printf("Age: %d, GPA: %.2lf\n", gpa, age);
+    printf("Age: %d, GPA: %.2f\n", age, gpa);
 
     return 0;
 }
 
 /**
 REASONING:
+CHANGE "GPA, AGE" into "AGE, GPA"
 
 
 */
