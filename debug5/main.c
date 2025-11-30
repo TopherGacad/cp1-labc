1ad5ae845c314617dfe9ccc3286cddaa1ed3819b
Mayeh <almazanmarielnicole@gmail.com>
2025-11-30 23:38:48 +0800

    done

--- debug5/main.c
+++ debug5/main.c
@@ -2,11 +2,12 @@
 
 int main() {
     double distance = 12.345;
-    printf("Distance: %d km\n", distance);
+    printf("Distance: %.3lf km\n", distance);
     return 0;
 }
 
 /**
 REASONING:
-
+1. Format is incorrect
+2. You need to set the decimal to three places using .3 since only three decimal places are needed
 */
