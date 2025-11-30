2dbcde432a7333ae4b73e17fb815d499f04460a0
kielelijahastillasa <kielelijahastillasa@gmail.com>
2025-11-30 17:09:09 +0800

    Revert "added debug-2"
    This reverts commit 30f4abb7902a8d6d0e2911250b0ae4c6a28a97ac.

--- debug2/main.c
+++ debug2/main.c
@@ -4,14 +4,13 @@ int main() {
     int age = 20;
     double gpa = 1.75;
 
-    printf("Age: %d, GPA: %.2f\n", age, gpa);
+    printf("Age: %d, GPA: %.2lf\n", gpa, age);
 
     return 0;
 }
 
 /**
 REASONING:
-CHANGE "GPA, AGE" into "AGE, GPA"
 
 
 */
