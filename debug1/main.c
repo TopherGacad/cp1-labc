9860dc374020019eaa0a2d9ae8c2d837c1e47faa
ycehara <mtchct@gmail.com>
2025-11-30 12:42:38 +0800

    created debug 1 to 5

--- debug1/main.c
+++ debug1/main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    string name = "Juan";
-    printf("Name: %s\n", name);
+int main()
+{
+    char stringname[6] = "Juan";
+    printf("Name: %s\n", stringname);
     return 0;
 }
 
 /**
 REASONING:
+    i. The data type must be specified.
+    ii. Variable name should not exceed with one word.
+    iii. You can also declare the maximum size the string can hold.
 
 
 */
