ca315987ec408fb095890eb80c9dae8465c4cbe2
kielelijahastillasa <kielelijahastillasa@gmail.com>
2025-11-30 17:09:49 +0800

    Revert "added debug-4"
    This reverts commit dc0d00dd91b2609ab84d26eaf34364b3a0842ff7.

--- debug4/main.c
+++ debug4/main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    char grade = 'A';
-    printf("Grade: %c\n", grade);
+    char grade = "A";
+    printf("Grade: %s\n", grade);
     return 0;
 }
 
 /**
 REASONING:
-Quotation mark should be single, since it has only 1 character
-format specifier should be %c
+
 
 */
