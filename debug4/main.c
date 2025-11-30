b6a147c60c8f3c1dea53c22324afd4a6f55350d8
Ibyennn <villanuevafrancdeivienm@gmail.com>
2025-11-30 19:15:20 +0800

    added debug-4

--- debug4/main.c
+++ debug4/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    char grade = "A";
-    printf("Grade: %s\n", grade);
+    char grade = 'A';
+    printf("Grade: %c\n", grade);
     return 0;
 }
 
 /**
 REASONING:
-
+The initial source code used double quotation
+The initial source code used "%s"
 
 */
