ad780eee1b060d9966c7f2feb6dcdf75169445e4
ycehara <mtchct@gmail.com>
2025-11-30 13:06:10 +0800

    created debug 1 to 5

--- debug4/main.c
+++ debug4/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    char grade = "A";
-    printf("Grade: %s\n", grade);
+    char grade = 'A';
+    printf("Grade: %c\n", grade);
     return 0;
 }
 
 /**
 REASONING:
-
+    i. You should only use single quotes when declaring the char data type.
+    ii. Format specifier should match type
 
 */
