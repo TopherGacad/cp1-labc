eea77850af24f3e055aaa41a1845a4fda95b11dd
kielelijahastillasa <kielelijahastillasa@gmail.com>
2025-11-30 18:19:22 +0800

    debug-2

--- debug2/main.c
+++ debug2/main.c
@@ -4,13 +4,13 @@ int main() {
     int age = 20;
     double gpa = 1.75;
 
-    printf("Age: %d, GPA: %.2lf\n", gpa, age);
+    printf("Age: %d, GPA: %.2lf\n", age, gpa);
 
     return 0;
 }
 
 /**
 REASONING:
-
+the order of variables must match the format specifiers
 
 */
