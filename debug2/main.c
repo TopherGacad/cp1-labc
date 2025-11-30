55ddc66781fad36757761868dd1d83355a6c0ee9
randelparco@gmail.com <randelparco@gmail.com>
2025-11-30 23:37:17 +0800

    added debug 2

--- debug2/main.c
+++ debug2/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-int main() {
+int main () {
     int age = 20;
     double gpa = 1.75;
 
-    printf("Age: %d, GPA: %.2lf\n", gpa, age);
+    printf("Age: %d, GPA: %lf\n", age, gpa);
 
     return 0;
 }
 
 /**
 REASONING:
-
+// Changed the format specifier for gpa from %d to %lf (or %f)
 
 */
