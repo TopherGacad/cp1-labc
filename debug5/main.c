a88f0fc2e981c0a699f63487b0193958638121bc
kielelijahastillasa <kielelijahastillasa@gmail.com>
2025-11-30 17:42:34 +0800

    added debug-5

--- debug5/main.c
+++ debug5/main.c
@@ -2,12 +2,12 @@
 
 int main() {
     double distance = 12.345;
-    printf("Distance: %d km\n", distance);
+    printf("Distance: %.3f km\n", distance);
     return 0;
 }
 
 /**
 REASONING:
-
+Changed the format specifier to %d to %.3f
 
 */
