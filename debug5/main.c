a9d8d6e50c4a0aefeb05a10ebc5b29b1fd3c492e
Ibyennn <villanuevafrancdeivienm@gmail.com>
2025-11-30 19:21:28 +0800

    added debug-5

--- debug5/main.c
+++ debug5/main.c
@@ -2,11 +2,11 @@
 
 int main() {
     double distance = 12.345;
-    printf("Distance: %d km\n", distance);
+    printf("Distance: %.3f km\n", distance);
     return 0;
 }
 
 /**
 REASONING:
-
+The format specifier is wrong, the format specifier of float is "%f"
 */
