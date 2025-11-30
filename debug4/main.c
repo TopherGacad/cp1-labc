3c341846eb955679c432666c399beda8688afa39
kielelijahastillasa <kielelijahastillasa@gmail.com>
2025-11-30 17:39:25 +0800

    added debug-4

--- debug4/main.c
+++ debug4/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    char grade = "A";
-    printf("Grade: %s\n", grade);
+    char grade = 'A';
+    printf("Grade: %c\n", grade);
     return 0;
 }
 
 /**
 REASONING:
-
+Quotation mark should be single since the letter is only 1
+Format specifier should be %c
 
 */
