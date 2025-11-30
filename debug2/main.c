3199b68e543388e1994506c71349306838663f56
Ibyennn <villanuevafrancdeivienm@gmail.com>
2025-11-30 19:02:00 +0800

    added debug-2

--- debug2/main.c
+++ debug2/main.c
@@ -4,13 +4,13 @@ int main() {
     int age = 20;
     double gpa = 1.75;
 
-    printf("Age: %d, GPA: %.2lf\n", gpa, age);
+    printf("Age: %d, GPA: %.2lf\n", age, gpa);
 
     return 0;
 }
 
 /**
 REASONING:
-
+The "AGE" and "GPA" is placed wrongly, it must match the order of the format specifier
 
 */
