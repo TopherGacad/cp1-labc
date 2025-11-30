417b07131ad4f6487c6f21edc094edb4808624b5
ycehara <mtchct@gmail.com>
2025-11-30 13:13:35 +0800

    created debug 1 to 5

--- debug5/main.c
+++ debug5/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    double distance = 12.345;
-    printf("Distance: %d km\n", distance);
+    float distance = 12.345;
+    printf("Distance: %.3f km\n", distance);
     return 0;
 }
 
 /**
 REASONING:
-
+    i. Used the float data type since the statement only represents 3 decimals
+    ii. Changed it into correct format specifier
 
 */
