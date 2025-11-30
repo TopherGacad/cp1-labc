85230e62f5f801323cec9393b7ea10aca7ceea9c
kielelijahastillasa <kielelijahastillasa@gmail.com>
2025-11-30 18:08:16 +0800

    revert

--- debug5/main.c
+++ debug5/main.c
@@ -8,12 +8,5 @@ int main() {
 
 /**
 REASONING:
-<<<<<<< HEAD
-<<<<<<< HEAD
-Changed the format specifier from "%d" to "%.3f"gi=
-=======
-=======
-Changed the format specifier from "%d" to "%.3f"
->>>>>>> parent of 117bbb1 (revert)
 
 */
