8ac9f1cdf35286d7e8bed51c19c49a9bc5b39cc8
myprcss <preciousvalle1012@gmail.com>
2025-11-30 11:43:44 +0800

    created a test_output and debug

--- debug5/main.c
+++ debug5/main.c
@@ -2,12 +2,13 @@
 
 int main() {
     double distance = 12.345;
-    printf("Distance: %d km\n", distance);
+    printf("Distance: %.3lf km\n", distance);
     return 0;
 }
 
 /**
 REASONING:
-
-
+1. The format specifier %d is used to print int.
+2. We should use %lf to print double.
+3. We can put .3 between % and lf to specify how many number to print after the decimal.
 */
