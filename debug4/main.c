37d150b0390787cc070a34699d9ace110e9493e7
randelparco@gmail.com <randelparco@gmail.com>
2025-11-30 23:53:33 +0800

    added debug 4

--- debug4/main.c
+++ debug4/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    char grade = "A";
-    printf("Grade: %s\n", grade);
+int main () {
+    char grade = 'A';
+    printf("Grade: %c\n", grade);
     return 0;
 }
 
 /**
 REASONING:
-
+// Changed the format specifier from %s to %c
 
 */
