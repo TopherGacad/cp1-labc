78cf05c4c7e2458118a70ebf84110c74a6d7f8a9
kei watanabe <kei.ashley.watanabe@gmail.com>
2025-12-01 09:00:35 +0800

    modified and debugged debug1

--- debug1/main.c
+++ debug1/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    string name = "Juan";
+    char name[20] = "Juan";
     printf("Name: %s\n", name);
     return 0;
 }
 
 /**
 REASONING:
-
+This is not the correct way to store a string in C as it is not considered a data type there.
+Instead, we use the data type char, create a variable, put [] alongside it in oder to store a string
+(the size can also be specified (e.g., [20])),
+and assign it a value (which in this case is "Juan").
 
 */
