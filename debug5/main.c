ec5ce596009e577d2155afa7b2dfdd4460983c1b
unknown <ctobias409@gmail.com>
2025-12-01 14:28:29 +0800

    minor changes in debug5

--- debug5/main.c
+++ debug5/main.c
@@ -8,7 +8,7 @@ int main() {
 
 /**
 REASONING:
-The decimal number 12. 345 has a space, which is invalid, and the format specifier %d is used for a double variable. 
-It should be written as 12.345 and printed using %lf (or %.3lf to control decimals) to display the value correctly.
+The format specifier %d is used for a double variable. 
+It should be printed using %lf (or %.3lf to control decimals) to display the value correctly.
 
 */
