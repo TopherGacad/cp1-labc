bb54185da41865acc4aa438c02df084a7f54e238
unknown <ctobias409@gmail.com>
2025-12-01 14:22:32 +0800

    minor changes in debug4

--- debug4/main.c
+++ debug4/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    char grade = "A";
-    printf("Grade: %s\n", grade);
+    char grade = 'A';
+    printf("Grade: %c\n", grade);
     return 0;
 }
 
 /**
 REASONING:
+When assigning a value to a char variable, you should use single quotes ('A') instead of double quotes. 
+Double quotes are used for strings, not single characters. 
+Also, the correct format specifier for a char is %c, not %s.
 
 
 */
