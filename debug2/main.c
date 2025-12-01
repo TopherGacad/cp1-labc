c7ee9002e0ad161320df986bf4d39f689b7d8468
unknown <ctobias409@gmail.com>
2025-12-01 14:09:04 +0800

    minor changes in debug2

--- debug2/main.c
+++ debug2/main.c
@@ -2,15 +2,16 @@
 
 int main() {
     int age = 20;
-    double gpa = 1.75;
+    double gpa = 1.25;
 
-    printf("Age: %d, GPA: %.2lf\n", gpa, age);
+    printf("Age: %d, GPA: %.2lf\n", age, gpa);
 
     return 0;
 }
 
 /**
 REASONING:
-
+The issue is the wrong order of format specifiers and variables in printf. 
+The solution is to match them correctly: %d for age and %.2f for gpa.
 
 */
