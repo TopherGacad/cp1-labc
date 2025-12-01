d5028f29f4ecfea29804445b7ae7b91f31ba0d21
unknown <ctobias409@gmail.com>
2025-12-01 13:39:02 +0800

    correct source code

--- debug1/main.c
+++ debug1/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    string name = "Juan";
+    char name[] = "Juan";
     printf("Name: %s\n", name);
     return 0;
 }
 
 /**
 REASONING:
-
+You shouldn’t use string as the data type for storing a word or a few characters. 
+Instead, you should use char with square brackets to create a character array and use the %s format specifier when printing it.
 
 */
