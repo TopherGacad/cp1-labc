285ca8dc147a60cfd56b13d0d1a540836859deea
Mariela Jayn Tundag <jayn.mawiealie@gmail.com>
2025-12-02 14:45:22 +0800

    created debug5_output

--- debug5/main.c
+++ debug5/main.c
@@ -9,6 +9,6 @@ int main() {
 /**
 REASONING:
 a. Since the distance does not require high precision, float can be used as variable.
-b. Using % is incorrect because that is used to print integers . The correct format
+b. Using %d is incorrect because that is used to print integers . The correct format
 specifier should be %.3fkm to print distance as it requires to display 3 decimal places.
 */
