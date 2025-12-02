af7f64eb1566f41c82f4ec929857453771ba7b0c
albert <cabilesalbertgian@gmail.com>
2025-12-02 22:07:14 +0800

    adding reasoning

--- debug5/main.c
+++ debug5/main.c
@@ -2,11 +2,11 @@
 
 int main() {
     double distance = 12.345;
-    printf("Distance: %d km\n", distance);
+    printf("Distance: %.3f km\n", distance);  // %.3f to show 3 decimal places
     return 0;
 }
-
 /**
-REASONING:
+REASONING:%d is for integers, distance is a double so use %.3lf to print with 3 decimal
 
-*/
+
+*/ 
