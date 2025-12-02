b13091736c49cf0939a68d780c7abf85d947aa32
unknown <betitojosephcarlo@gmail.com>
2025-12-02 19:19:09 +0800

    updated all remaining debug

--- debug4/main.c
+++ debug4/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    char grade = "A";
-    printf("Grade: %s\n", grade);
+    char grade = 'A';
+    printf("Grade: %c\n", grade);
     return 0;
 }
 
 /**
 REASONING:
-
+    1. Replaced the double quotation, with a single quotation.
+    2. The format specifier should be %c.
 
 */
--- debug5/main.c
+++ debug5/main.c
@@ -2,11 +2,11 @@
 
 int main() {
     double distance = 12.345;
-    printf("Distance: %d km\n", distance);
+    printf("Distance: %.3f km\n", distance);
     return 0;
 }
 
 /**
 REASONING:
-
+    Replaced the format specifier with %.3f
 */
