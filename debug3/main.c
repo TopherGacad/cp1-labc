009fb33d11aab02f63c7546e2bc9428834abb28c
GabTaguinod <gabtaguinod09@gmail.com>
2025-12-02 23:44:47 +0800

    late

--- debug3/main.c
+++ debug3/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
@@ -9,6 +8,6 @@ int main()
 
 /**
 REASONING:
-
+<stdlib.h> is included but not required but its no function in this program.
 
 */
