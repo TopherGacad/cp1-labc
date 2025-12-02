5e28e5413ba0ec7ee99ef66ba5a8b543731239ea
Symon Sibala <symn.sibala@gmail.com>
2025-12-02 21:20:27 +0800

    fixed debug1

--- debug1/main.c
+++ debug1/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    string name = "Juan";
+    char name[] = "Juan";
     printf("Name: %s\n", name);
     return 0;
 }
 
 /**
 REASONING:
-
+there is no string in C language, instead we use character arrays (char[]).
 
 */
