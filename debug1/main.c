54ca8625f5cb54765a5358feb76382422cda1ec2
unknown <betitojosephcarlo@gmail.com>
2025-12-02 18:44:22 +0800

    updated debug1

--- debug1/main.c
+++ debug1/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    string name = "Juan";
+    char name[] = "Juan";
     printf("Name: %s\n", name);
     return 0;
 }
 
 /**
 REASONING:
-
+    1. Because string is not recognized in C language, you’ll need to use char as your data type.
 
 */
