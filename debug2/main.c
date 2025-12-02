4a0b3759b0c166be8ca39ebbc09c5eb67fe65452
unknown <betitojosephcarlo@gmail.com>
2025-12-02 18:58:20 +0800

    updated debug2

--- debug2/main.c
+++ debug2/main.c
@@ -4,13 +4,13 @@ int main() {
     int age = 20;
     double gpa = 1.75;
 
-    printf("Age: %d, GPA: %.2lf\n", gpa, age);
+    printf("Age: %d, GPA: %.2f\n", age, gpa);
 
     return 0;
 }
 
 /**
 REASONING:
-
+    1. The data specifier is placed wrong it should be "age, gpa"
 
 */
