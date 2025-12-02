69f3becfa485d4bee4ca3094ed8ce52354c14a88
renz-02 <minalinrenz@gmail.com>
2025-12-02 19:04:36 +0800

    output debug

--- debug4/main.c
+++ debug4/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    char grade = "A";
-    printf("Grade: %s\n", grade);
+    char grade = 'A';
+    printf("Grade: %c", grade);
     return 0;
 }
 
 /**
-REASONING:
+REASONING: 1. it should use %c format specifier instead of %s\n because grade is a single character
+2. the grade should be enclosed in single quotes ' ' instead of double quotes " "
 
 
 */
