66c032bb6c391144b9321abe8d66a8e9882f9739
Judiel Bencio <judielbencio@gmail.com>
2025-12-03 12:01:21 +0800

    debug and modify 1-5

--- debug5/main.c
+++ debug5/main.c
@@ -2,11 +2,13 @@
 
 int main() {
     double distance = 12.345;
-    printf("Distance: %d km\n", distance);
+    printf("Distance: %.3lf km\n", distance);
     return 0;
 }
 
 /**
 REASONING:
+1. Changing the %d into %f indicating that it is with decimal places
+2. added the printf ("Distance: %.3lf km\n", distance);
 
 */
